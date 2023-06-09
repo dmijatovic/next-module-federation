4f8930fc013b85c0917e26dbe09f53627a7dcaf1
Dusan Mijatovic (PC2022) <d.mijatovic@dv4all.nl>
2023-06-09 20:10:38 +0200

    feat: initial scafold software app

---
+++ software-app/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Roboto } from 'next/font/google'
+
+const inter = Roboto({ subsets: ['latin'],weight:['100','300','400','500'] })
+
+export const metadata = {
+  title: 'Software app',
+  description: 'This is RSD software section',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`flex flex-col ${inter.className}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ software-app/app/page.tsx
@@ -0,0 +1,11 @@
+
+export default function SoftwareOverview() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-3xl my-4">Software overview page</h1>
+      <p>
+        This app contains software over page and subsequent software pages.
+      </p>
+    </main>
+  )
+}
